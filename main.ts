336090184794cd047b967ae2326b1f609ccf45e9
metaladdict <metaladdict@users.noreply.github.com>
2023-05-13 01:47:46 +0200

    Pas de ClearBuffer - meilleur perf

--- main.ts
+++ main.ts
@@ -12,25 +12,24 @@ input.onButtonPressed(Button.B, function () {
     }
     setMv();
 })
-let mv=[1,0]
+let mv = [1, 0]
 let lims: number[] = []
 let lstPts: number[][] = []
 let direction = 0
 direction = 1
 let xtete = 80
 let ytete = 60
-let nbPts = 10
+let nbPts = 8
 LCD1IN8.LCD_Init()
 LCD1IN8.LCD_ClearBuf()
 LCD1IN8.LCD_Clear()
 LCD1IN8.LCD_SetBL(536)
 for (let i = 0; i <= nbPts - 1; i++) {
-    lstPts.push([xtete-2*i, ytete])
+    lstPts.push([xtete - 2 * i, ytete])
 }
-function setMv()
-{
+function setMv() {
     if (direction == 0) {
-        mv = [0,2]
+        mv = [0, 2]
     } else if (direction == 1) {
         mv = [2, 0]
     } else if (direction == 2) {
@@ -40,21 +39,23 @@ function setMv()
     }
 }
 basic.forever(function () {
-    LCD1IN8.LCD_ClearBuf()
+    //LCD1IN8.LCD_ClearBuf()
+    
     LCD1IN8.DrawPoint(
-        lstPts[nbPts-1][0],
-        lstPts[nbPts-1][1],
+        lstPts[nbPts - 1][0],
+        lstPts[nbPts - 1][1],
         0xffff,
         DOT_PIXEL.DOT_PIXEL_2
     )
-    xtete+=mv[0]
-    ytete+=mv[1]
+    
+    xtete += mv[0]
+    ytete += mv[1]
     lstPts.unshift([xtete, ytete])
     lims = [
-    160,
-    128,
-    0,
-    0
+        160,
+        128,
+        0,
+        0
     ]
 
     for (let i = 0; i <= nbPts; i++) {
@@ -63,22 +64,22 @@ basic.forever(function () {
         lims[2] = Math.max(lims[2], lstPts[i][0])
         lims[3] = Math.max(lims[3], lstPts[i][1])
 
-        if(i<nbPts)
-        {
+        if (i < nbPts) {
             LCD1IN8.DrawPoint(
-            lstPts[i][0],
-            lstPts[i][1],
-            0,
-            DOT_PIXEL.DOT_PIXEL_2
+                lstPts[i][0],
+                lstPts[i][1],
+                0,
+                DOT_PIXEL.DOT_PIXEL_2
             )
         }
     }
     lstPts.pop()
+    
     LCD1IN8.LCD_DisplayWindows(
-    lims[0],
-    lims[1],
-    lims[2] + 2,
-    lims[3] + 2
+        lims[0],
+        lims[1],
+        lims[2] + 2,
+        lims[3] + 2
     )
-    console.log(lstPts.length)
+    
 })
