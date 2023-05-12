7a3473ddb4666548ccc67a0fab9113b3c19640cc
metaladdict <metaladdict@users.noreply.github.com>
2023-05-13 01:30:40 +0200

    Centré - clear sur l'arrière

--- main.ts
+++ main.ts
@@ -17,26 +17,26 @@ let lims: number[] = []
 let lstPts: number[][] = []
 let direction = 0
 direction = 1
-let ytete = 20
-let xtete = 20
-let nbPts = 15
+let xtete = 80
+let ytete = 60
+let nbPts = 10
 LCD1IN8.LCD_Init()
 LCD1IN8.LCD_ClearBuf()
 LCD1IN8.LCD_Clear()
 LCD1IN8.LCD_SetBL(536)
 for (let i = 0; i <= nbPts - 1; i++) {
-    lstPts.push([xtete-1, ytete])
+    lstPts.push([xtete-2*i, ytete])
 }
 function setMv()
 {
     if (direction == 0) {
-        mv = [0,1]
+        mv = [0,2]
     } else if (direction == 1) {
-        mv = [1, 0]
+        mv = [2, 0]
     } else if (direction == 2) {
-        mv = [0, -1]
+        mv = [0, -2]
     } else if (direction == 3) {
-        mv = [-1, 0]
+        mv = [-2, 0]
     }
 }
 basic.forever(function () {
@@ -47,7 +47,6 @@ basic.forever(function () {
         0xffff,
         DOT_PIXEL.DOT_PIXEL_2
     )
-    lstPts.pop()
     xtete+=mv[0]
     ytete+=mv[1]
     lstPts.unshift([xtete, ytete])
@@ -58,18 +57,23 @@ basic.forever(function () {
     0
     ]
 
-    for (let i = 0; i <= nbPts - 1; i++) {
+    for (let i = 0; i <= nbPts; i++) {
         lims[0] = Math.min(lims[0], lstPts[i][0])
         lims[1] = Math.min(lims[1], lstPts[i][1])
         lims[2] = Math.max(lims[2], lstPts[i][0])
         lims[3] = Math.max(lims[3], lstPts[i][1])
-        LCD1IN8.DrawPoint(
-        lstPts[i][0],
-        lstPts[i][1],
-        0,
-        DOT_PIXEL.DOT_PIXEL_2
-        )
+
+        if(i<nbPts)
+        {
+            LCD1IN8.DrawPoint(
+            lstPts[i][0],
+            lstPts[i][1],
+            0,
+            DOT_PIXEL.DOT_PIXEL_2
+            )
+        }
     }
+    lstPts.pop()
     LCD1IN8.LCD_DisplayWindows(
     lims[0],
     lims[1],
